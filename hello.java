ee2e8c4cd17ebcd55db6c4716d3e0b932d998366
Training_C2a.05.01 <Training_C2a.05.01@accenture.com>
2018-03-29 13:02:09 +0530

    new class file is created

--- hello.java
+++ hello.java
@@ -1,7 +1,6 @@
-public class Hello{
-public Static void main(String[] args){
-
-System.out.Println("Hello world");
-
-}
+public class Hello {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
+   }
 }
